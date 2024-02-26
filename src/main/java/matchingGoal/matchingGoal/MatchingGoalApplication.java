9409f0d9639b65db714505ee69e49ea5826c4b6c
hana <sjfn242@gmail.com>
2024-02-26 19:38:21 +0900

    add skeleton files

---
+++ src/main/java/matchingGoal/matchingGoal/MatchingGoalApplication.java
@@ -0,0 +1,13 @@
+package matchingGoal.matchingGoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingGoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchingGoalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/matchingGoal/matchingGoal/MatchingGoalApplicationTests.java
@@ -0,0 +1,13 @@
+package matchingGoal.matchingGoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingGoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
