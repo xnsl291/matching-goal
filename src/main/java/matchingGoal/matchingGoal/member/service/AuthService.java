db0eb9a0e06886dc170c0a82f238136ef6cfbbf2
hana <sjfn242@gmail.com>
2024-03-11 21:51:23 +0900

    fix : [#1] checkPwFormat 함수, @Password 혼용 -> @Password로 통일

--- src/main/java/matchingGoal/matchingGoal/member/service/AuthService.java
+++ src/main/java/matchingGoal/matchingGoal/member/service/AuthService.java
@@ -32,15 +32,6 @@ public class AuthService {
     private final String TOKEN_PREFIX = "RT_";
 
     /**
-     * 비밀번호 포맷 검증
-     * @param password - 비밀번호
-     * @return 포맷 일치 여부
-     */
-    public boolean checkPwFormat(String password){
-        return password.matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d!@#$%^&*]{10,}$");
-    }
-
-    /**
      * 회원가입
      * @param registerDto - 회원가입 dto
      * @return "회원가입 성공"
@@ -55,10 +46,6 @@ public class AuthService {
         // 닉네임 중복 확인
         if (! checkNickname(registerDto.getNickname()))
             throw new DuplicatedNicknameException(ErrorCode.DUPLICATED_NICKNAME);
-
-        // 비밀번호 포맷 검증 : 10자 이상, 알파벳 & 숫자 필수, 특수문자( !@#$%^&*) 입력 가능
-        if (! checkPwFormat(registerDto.getPassword()))
-            throw new InvalidPasswordFormatException(ErrorCode.INVALID_PASSWORD_FORMAT);
 
         Member member = Member.builder()
                 .name(registerDto.getName())
@@ -155,10 +142,9 @@ public class AuthService {
      * @return "변경완료"
      */
     public String updatePassword(UpdatePwDto updatePwDto) {
-        Member member = memberRepository.findById(updatePwDto.getId()).orElseThrow(() -> new MemberNotFoundException(ErrorCode.MEMBER_NOT_EXISTS));
+        Member member = memberRepository.findById(updatePwDto.getId())
+                .orElseThrow(() -> new MemberNotFoundException(ErrorCode.MEMBER_NOT_EXISTS));
 
-        if (! checkPwFormat(updatePwDto.getNewPassword()))
-            throw new InvalidPasswordFormatException(ErrorCode.INVALID_PASSWORD_FORMAT);
         member.setPassword(updatePwDto.getNewPassword());
         return "변경완료";
     }
