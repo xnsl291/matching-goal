6729a2266a62e524ac61ae9bba14e96fa3225c4a
NotBii <costic1375@gmail.com>
2024-03-13 19:44:23 +0900

    member 연동, dto <-> entity 전환을 각 dto, entity에서 메소드로 관리

--- src/main/java/matchingGoal/matchingGoal/common/config/SecurityConfig.java
+++ src/main/java/matchingGoal/matchingGoal/common/config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package matchingGoal.matchingGoal.common.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -18,16 +25,16 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorizeRequests) ->
-                        authorizeRequests
-                                .requestMatchers("/", "api/auth/sign-in").permitAll()
-                                .requestMatchers( "api/**").permitAll()  /// todo: need to be specified!
-                                .anyRequest().authenticated()
-                )
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests((authorizeRequests) ->
+//                        authorizeRequests
+//                                .requestMatchers("/", "api/auth/sign-in").permitAll()
+//                                .requestMatchers( "api/**").permitAll()  /// todo: need to be specified!
+//                                .anyRequest().authenticated()
+//                )
 
                 /**
                 .formLogin((formLogin) ->
@@ -40,7 +47,31 @@ public class SecurityConfig {
                 )
                  **/
                 ;
+//
+//        return http.build();
+//    }
 
-        return http.build();
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                @Override
+                public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setMaxAge(3600L); //1시간
+                    return config;
+                }
+
+            }))
+            .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/").authenticated()
+                .requestMatchers("/api/auth/sign-in", "api/**").permitAll())
+            .formLogin(Customizer.withDefaults())
+            .httpBasic(Customizer.withDefaults())
+            .build();
     }
 }
