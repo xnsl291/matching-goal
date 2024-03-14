3c1fb7dc5c113686d18e8f4542702ed16920c67c
NotBii <costic1375@gmail.com>
2024-03-14 17:29:53 +0900

    stomp & cors 수정

--- src/main/java/matchingGoal/matchingGoal/common/config/SecurityConfig.java
+++ src/main/java/matchingGoal/matchingGoal/common/config/SecurityConfig.java
@@ -1,20 +1,13 @@
 package matchingGoal.matchingGoal.common.config;
 
-import jakarta.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -25,53 +18,38 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests((authorizeRequests) ->
-//                        authorizeRequests
-//                                .requestMatchers("/", "api/auth/sign-in").permitAll()
-//                                .requestMatchers( "api/**").permitAll()  /// todo: need to be specified!
-//                                .anyRequest().authenticated()
-//                )
-
-                /**
-                .formLogin((formLogin) ->
-                        formLogin
-                                .loginPage("/api/auth/sign-in")
-                                .usernameParameter("username")
-                                .passwordParameter("password")
-                                .loginProcessingUrl("/api/auth/sign-in")
-                                //.defaultSuccessUrl("/", true)
-                )
-                 **/
-                ;
-//
-//        return http.build();
-//    }
-
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-            .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-                @Override
-                public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                    CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
-                    config.setAllowedMethods(Collections.singletonList("*"));
-                    config.setAllowCredentials(true);
-                    config.setAllowedHeaders(Collections.singletonList("*"));
-                    config.setMaxAge(3600L); //1시간
-                    return config;
-                }
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-            }))
-            .authorizeHttpRequests((requests) -> requests.anyRequest().permitAll())
-//                .requestMatchers("/**").authenticated()
-//                .requestMatchers("/api/auth/sign-in", "api/**").permitAll())
-//            .formLogin(Customizer.withDefaults())
-//            .httpBasic(Customizer.withDefaults())
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.addAllowedOrigin("*");
+//        corsConfiguration.addAllowedMethod("*");
+//        corsConfiguration.addAllowedHeader("*");
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+//            .cors().configurationSource(source).and()
+            .cors(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/**", "/api/auth/sign-in").permitAll()
+                .requestMatchers("/api/**").permitAll())
             .build();
+//
+//  .formLogin((formLogin) ->
+//          formLogin
+//                  .loginPage("/api/auth/sign-in")
+//                  .usernameParameter("username")
+//                   .passwordParameter("password")
+//                   .loginProcessingUrl("/api/auth/sign-in")
+//                  .defaultSuccessUrl("/", true)
+//  )
+
+
+
     }
+
+
+
 }
