863058beed1adc2f200b6b975bbf013d6129b7f5
NotBii <costic1375@gmail.com>
2024-03-19 17:43:35 +0900

    알림 서비스 작성 알림 생성(token, alarmType, contentId) : 토큰에서 memberId를 받고 type 및 관련 컨텐츠 id를 저장. 알림 조회(token) : 토큰에서 받은 id로 db에서 알림 리스트 조회, type에 따라 메세지를 세팅해서 반환 알림 확인 : isCheckedOut = true로 수정

--- src/main/java/matchingGoal/matchingGoal/alarm/domain/entity/Alarm.java
+++ src/main/java/matchingGoal/matchingGoal/alarm/domain/entity/Alarm.java
@@ -1,33 +1,39 @@
-package matchingGoal.matchingGoal.alarm.entity;
+package matchingGoal.matchingGoal.alarm.domain.entity;
 
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Builder.Default;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import matchingGoal.matchingGoal.alarm.domain.AlarmType;
+import matchingGoal.matchingGoal.alarm.domain.AlarmTypeAttributeConverter;
 import matchingGoal.matchingGoal.alarm.dto.AlarmDto;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Alarm {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
   private long memberId;
   @Convert(converter = AlarmTypeAttributeConverter.class)
-  private String type;
+  private AlarmType type;
   private long contentId;
-  private String message;
-  private boolean checkedOut;
+  @Default
+  private boolean checkedOut = false;
   @CreatedDate
   private LocalDateTime createdDate;
 
@@ -36,9 +42,14 @@ public class Alarm {
         .memberId(dto.getMemberId())
         .type(dto.getType())
         .contentId(dto.getContentId())
-        .message(dto.getMessage())
         .checkedOut(dto.isCheckedOut())
         .build();
   }
+
+  public void checkOut() {
+    this.checkedOut = true;
+
+  }
+
 }
 
--- src/main/java/matchingGoal/matchingGoal/alarm/repository/AlarmRepository.java
+++ src/main/java/matchingGoal/matchingGoal/alarm/repository/AlarmRepository.java
@@ -1,10 +1,11 @@
 package matchingGoal.matchingGoal.alarm.repository;
 
-import matchingGoal.matchingGoal.alarm.entity.Alarm;
+import java.util.List;
+import matchingGoal.matchingGoal.alarm.domain.entity.Alarm;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
-
+  List<Alarm> findAllByMemberId(long memberId);
 }
