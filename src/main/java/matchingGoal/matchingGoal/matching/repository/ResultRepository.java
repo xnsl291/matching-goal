38d163cf0ecece37c6d704aaa064cbf9ae4ec26d
NotBii <costic1375@gmail.com>
2024-03-21 20:41:53 +0900

    ResultRepository에 findByGameId(long) 추가 MemberService 예외처리 부분 수정

--- src/main/java/matchingGoal/matchingGoal/matching/repository/ResultRepository.java
+++ src/main/java/matchingGoal/matchingGoal/matching/repository/ResultRepository.java
@@ -1,5 +1,6 @@
 package matchingGoal.matchingGoal.matching.repository;
 
+import java.util.Optional;
 import matchingGoal.matchingGoal.matching.domain.entity.Game;
 import matchingGoal.matchingGoal.matching.domain.entity.Result;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface ResultRepository extends JpaRepository<Result, Long> {
 
   boolean existsByGame(Game game);
+  Optional<Result> findByGameId(long gameId);
 }
