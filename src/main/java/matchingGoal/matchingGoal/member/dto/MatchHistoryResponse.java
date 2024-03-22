8b8ccb7d99a8ef92d7201841f15bfd96ec18ee72
hana <sjfn242@gmail.com>
2024-03-22 16:42:53 +0900

    refactor : MatchHistoryResponse에서 isWin 판별 로직 수정

--- src/main/java/matchingGoal/matchingGoal/member/dto/MatchHistoryResponse.java
+++ src/main/java/matchingGoal/matchingGoal/member/dto/MatchHistoryResponse.java
@@ -28,12 +28,16 @@ public class MatchHistoryResponse {
         Game game = result.getGame();
         Member opponent = game.getTeam1().getId().equals(member.getId()) ? game.getTeam2() : game.getTeam1();
 
+        boolean isMemberWin = result.getScore1() > result.getScore2() ;  // 글 작성자가 이겼는지?
+        if (game.getTeam2().getId().equals(member.getId()) ) // 글 작성자 != 멤버 인지 확인
+            isMemberWin = !isMemberWin ;
+
         return MatchHistoryResponse.builder()
                 .resultId(result.getId())
                 .opponentId(opponent.getId())
                 .opponentImgUrl(opponent.getImageUrl())
                 .opponentNickname(opponent.getNickname())
-                .isWin(result.getWinner().getId().equals(member.getId()))
+                .isWin(isMemberWin)
                 .score1(result.getScore1())
                 .score2(result.getScore2())
                 .date(game.getDate())
