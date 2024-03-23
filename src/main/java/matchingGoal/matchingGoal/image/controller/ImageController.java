3a9ed38ce512d34f1face476357abb1ac74963ec
hana <sjfn242@gmail.com>
2024-03-23 14:41:15 +0900

    chore : 필요없는 주석 삭제

--- src/main/java/matchingGoal/matchingGoal/image/controller/ImageController.java
+++ src/main/java/matchingGoal/matchingGoal/image/controller/ImageController.java
@@ -24,17 +24,4 @@ public class ImageController {
     public ResponseEntity<Boolean> removeImage(@Valid @RequestParam("imageUrl") String imageUrl){
         return ResponseEntity.ok().body(imageService.removeImage(imageUrl));
     }
-
-//    @PostMapping("/matching")
-//    public ResponseEntity<String> uploadBoardImage(
-//            @RequestHeader(name = AUTH_HEADER) String token,
-//           @Valid @RequestParam("file") MultipartFile file ,
-//           @RequestParam(value="matchingId", required = false, defaultValue="0") Long matchingId){
-//        return ResponseEntity.ok().body(imageService.uploadBoardImage(file));
-//    }
-
-//        @DeleteMapping("/matching")
-//    public ResponseEntity<String> removeImage(@PathParam ("image-id") Long imageId , @RequestBody DeleteBoardImageDto dto){
-//        return ResponseEntity.ok().body(imageService.removeBoardImage(imageId));
-//    }
 }
