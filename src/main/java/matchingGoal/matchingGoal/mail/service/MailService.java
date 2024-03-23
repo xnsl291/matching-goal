cc84714a961923a533fa729511d738d0df60a7c8
hana <sjfn242@gmail.com>
2024-03-23 14:50:41 +0900

    refactor : 인증메일 발송시 파라미터 요청방법 변환 (@RequestParam String email -> @RequestBody SendMailVerificationDto dto)

--- src/main/java/matchingGoal/matchingGoal/mail/service/MailService.java
+++ src/main/java/matchingGoal/matchingGoal/mail/service/MailService.java
@@ -10,6 +10,7 @@ import matchingGoal.matchingGoal.common.service.RedisService;
 import matchingGoal.matchingGoal.common.type.ErrorCode;
 import matchingGoal.matchingGoal.mail.dto.MailDto;
 import matchingGoal.matchingGoal.mail.dto.MailVerificationDto;
+import matchingGoal.matchingGoal.mail.dto.SendMailVerificationDto;
 import matchingGoal.matchingGoal.mail.model.entity.EmailContent;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.jsoup.Jsoup;
@@ -47,10 +48,9 @@ public class MailService {
 
     /**
      * 이메일 인증 메일 발송
-     * @param email - 이메일
      * @return 발송성공여부 (성공시 true)
      */
-    public Boolean sendVerificationMail(String email) {
+    public Boolean sendVerificationMail(SendMailVerificationDto sendMailVerificationDto) {
         final long redisDurationInMinutes = 3;
         final String TEMPLATE_NAME = "InitialEmailVerification.html";
         final String code = RandomStringUtils.random(7, true, true);
@@ -61,10 +61,10 @@ public class MailService {
         emailContent.add("code", code);
         emailContent.add("expire", expire);
 
-        if (!sendMail(email, TEMPLATE_NAME, emailContent))
+        if (!sendMail(sendMailVerificationDto.getEmail(), TEMPLATE_NAME, emailContent))
             return false;
 
-        redisService.setData(VALID_PREFIX + email, code, redisDurationInMinutes);
+        redisService.setData(VALID_PREFIX + sendMailVerificationDto.getEmail(), code, redisDurationInMinutes);
         return true;
     }
 
