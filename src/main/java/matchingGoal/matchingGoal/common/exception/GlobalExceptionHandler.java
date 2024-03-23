7b1b37b87cfc653e80d71b33c6a6cabf39d8a50a
hana <sjfn242@gmail.com>
2024-03-23 23:00:40 +0900

    refactor : ErrorResponse에 에러메세지 반환하는 항목 'errors' 추가

--- src/main/java/matchingGoal/matchingGoal/common/exception/GlobalExceptionHandler.java
+++ src/main/java/matchingGoal/matchingGoal/common/exception/GlobalExceptionHandler.java
@@ -1,10 +1,15 @@
 package matchingGoal.matchingGoal.common.exception;
 
 import matchingGoal.matchingGoal.common.type.ErrorCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler  {
@@ -16,6 +21,17 @@ public class GlobalExceptionHandler  {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException e) {
-        return ErrorResponse.toResponseEntity(ErrorCode.INVALID_ARGUMENT);
+        Map<String, String> error = new HashMap<>();
+        e.getBindingResult().getAllErrors()
+                .forEach(c -> {
+                    error.put( ((FieldError) c).getField(), c.getDefaultMessage());
+                });
+
+        return ResponseEntity.badRequest().body(ErrorResponse.builder()
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .error(HttpStatus.BAD_REQUEST.name())
+                        .code(ErrorCode.INVALID_ARGUMENT.toString())
+                        .errors(error)
+                .build());
     }
 }
