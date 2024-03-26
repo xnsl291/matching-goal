d5623953ad92906e031308ce1a3b2a390821d3c6
hana <sjfn242@gmail.com>
2024-03-26 15:15:56 +0900

    refactor : response에 builder 추가

--- src/main/java/matchingGoal/matchingGoal/member/dto/SignInResponse.java
+++ src/main/java/matchingGoal/matchingGoal/member/dto/SignInResponse.java
@@ -1,6 +1,8 @@
 package matchingGoal.matchingGoal.member.dto;
 
 import lombok.*;
+import matchingGoal.matchingGoal.common.auth.JwtToken;
+import matchingGoal.matchingGoal.member.model.entity.Member;
 
 @Getter
 @Setter
@@ -19,4 +21,15 @@ public class SignInResponse {
     private String nickname;
 
     private String imageUrl;
+
+    public static SignInResponse of(JwtToken token, Member member) {
+        return SignInResponse.builder()
+                .accessToken(token.getAccessToken())
+                .refreshToken(token.getRefreshToken())
+                .memberId(member.getId())
+                .email(member.getEmail())
+                .nickname(member.getNickname())
+                .imageUrl(member.getImageUrl())
+                .build();
+    }
 }
