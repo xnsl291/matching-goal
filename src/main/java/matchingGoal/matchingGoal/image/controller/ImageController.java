06c09759f532577c52a67d59207c222a54b2536e
hana <sjfn242@gmail.com>
2024-03-28 18:36:33 +0900

    feat : 이미지 S3 업로드

--- src/main/java/matchingGoal/matchingGoal/image/controller/ImageController.java
+++ src/main/java/matchingGoal/matchingGoal/image/controller/ImageController.java
@@ -2,7 +2,6 @@ package matchingGoal.matchingGoal.image.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import matchingGoal.matchingGoal.image.dto.UploadImageResponse;
 import matchingGoal.matchingGoal.image.service.ImageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,15 +12,23 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api/images")
 public class ImageController {
     private final ImageService imageService;
-    private static final String AUTH_HEADER = "Authorization";
 
+    /**
+     * 이미지 업로드
+     * @return imageUrl
+     */
     @PostMapping("/upload")
     public ResponseEntity<String> uploadImage( @Valid @RequestParam("file") MultipartFile file){
         return ResponseEntity.ok().body(imageService.uploadImage(file));
     }
 
+    /**
+     * 이미지 삭제
+     * @return "삭제완료"
+     */
     @DeleteMapping("/remove")
-    public ResponseEntity<Boolean> removeImage(@Valid @RequestParam("imageUrl") String imageUrl){
-        return ResponseEntity.ok().body(imageService.removeImage(imageUrl));
+    public ResponseEntity<String> removeImage(@Valid @RequestParam("imageUrl") String imageUrl){
+        imageService.removeImage(imageUrl);
+        return ResponseEntity.ok().body("삭제완료");
     }
 }
