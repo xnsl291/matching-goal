e3692e932a1cc9a9d580c8740caa7345460436e8
NotBii <costic1375@gmail.com>
2024-03-28 19:31:14 +0900

    main

--- src/main/java/matchingGoal/matchingGoal/mail/dto/SendMailVerificationDto.java
+++ src/main/java/matchingGoal/matchingGoal/mail/dto/SendMailVerificationDto.java
@@ -2,13 +2,17 @@ package matchingGoal.matchingGoal.mail.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SendMailVerificationDto {
   @Email
   @NotBlank(message = "이메일을 입력해주세요")
